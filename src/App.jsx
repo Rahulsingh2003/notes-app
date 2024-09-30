a6ba76c6d22554163e1e74bd3e8868be4a39a168
Rahul <rs9462784650@gmail.com>
2024-09-30 15:39:05 +0530

    Finally starting the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  console.log("object");
+
+  return (
+    <div>
+      <p>Rahul Here!!</p>
+    </div>
+  );
+}
+
+export default App;
