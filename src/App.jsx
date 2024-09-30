e6f4f79a743af297b92b8bf790251f37aa76657c
Rahul <rs9462784650@gmail.com>
2024-09-30 16:16:23 +0530

    updating demo pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
 import { useState } from "react";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Paste from "./components/Paste";
+import Home from "./components/Home";
+import ViewPaste from "./components/viewPaste";
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:
+      <div>
+      <Navbar/>
+      <Home />
+
+      </div>
+    },
+    {
+      path:"/pastes",
+      element:
+      <div>
+      <Navbar />
+      <Paste />
+
+      </div>
+
+    },{
+      path:"/pastes/:id",
+      element:
+      <div>
+      <Navbar />
+      <ViewPaste />
+
+      </div>
+    }
+  ]
+)
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log("object");
 
   return (
     <div>
-      <p>Rahul Here!!</p>
+      <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ src/components/ViewPaste.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewPaste = () => {
+  return <div>viewPaste</div>;
+};
+
+export default ViewPaste;
