9bdd8c6c14d74221c245791478416c51dd8a0f1c
Rahul <rs9462784650@gmail.com>
2024-10-01 00:21:56 +0530

    delete

--- src/components/Paste.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Paste = () => {
-  return <div> List of Paste</div>;
-};
-
-export default Paste;
--- src/components/ViewPaste.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ViewPaste = () => {
-  return <div>viewPaste</div>;
-};
-
-export default ViewPaste;
